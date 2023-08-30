318207ac9a66a0111bbd67a0cbaeb97e23713ecb
Chris Hillery <ceej@couchbase.com>
2023-08-30 00:07:21 -0700

    CBD-5570: Couchbase-specific build script
    Change-Id: I420adf6fd72c2cbc101baad46c1b051ab4e1f7c4
    Reviewed-on: https://review.couchbase.org/c/kafka-connect-mongo/+/196352
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ritik Raj <raj.ritik9835@gmail.com>

---
+++ couchbase-build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+MVN_VERSION=3.9.2
+
+usage() {
+    echo "Usage: $0 -p PRODUCT -r RELEASE -v VERSION -b BLD_NUM"
+    exit 1
+}
+
+while getopts ":p:r:v:b:h?" opt; do
+    case $opt in
+        p) PRODUCT=$OPTARG ;;
+        r) RELEASE=$OPTARG ;;
+        v) VERSION=$OPTARG ;;
+        b) BLD_NUM=$OPTARG ;;
+        h|?) usage ;;
+        :) echo "-${OPTARG} requires an argument"
+           usage
+           ;;
+    esac
+done
+
+if [ -z "${PRODUCT}" -o -z "${RELEASE}" -o \
+     -z "${VERSION}" -o -z "${BLD_NUM}" ]; then
+    usage
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${SCRIPT_DIR}"
+
+if ! type mvn >& /dev/null; then
+    TOOLS_DIR="$(pwd)/../tools"
+    mkdir -p "${TOOLS_DIR}"
+    cbdep install -d "${TOOLS_DIR}" mvn ${MVN_VERSION}
+    export PATH=${TOOLS_DIR}/mvn-${MVN_VERSION}/bin:${PATH}
+fi
+
+PRODUCT_VERSION="${VERSION}.${BLD_NUM}"
+mvn versions:set -DnewVersion="${PRODUCT_VERSION}"
+mvn clean verify -Dquick
+
+# Place desired output jars into dist/ directory at root of repo sync.
+# Update this script if the set of desired jars change.
+DIST_DIR="$(pwd)/../dist"
+mkdir -p "${DIST_DIR}"
+cp \
+    debezium-api/target/debezium-api-${PRODUCT_VERSION}.jar \
+    debezium-connector-mongodb/target/debezium-connector-mongodb-${PRODUCT_VERSION}.jar \
+    debezium-core/target/debezium-core-${PRODUCT_VERSION}.jar \
+    "${DIST_DIR}"
